44c966f2f2edee988c9afa8f26a217ea0d16a3b0
Ishita <ajeets1900@gmail.com>
2024-02-27 00:36:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import Home from './components/home/home'
+
+function App() {
+
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
